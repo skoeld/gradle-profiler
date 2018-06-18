2e8861cd02fbdf398b38f0428b49834627e6da26
Magnus Sköld <mask@spotify.com>
2018-06-18 10:52:46 -0400

    Support mutation of flat android manifests

--- src/main/java/org/gradle/profiler/mutations/ApplyChangeToAndroidManifestFileMutator.java
+++ src/main/java/org/gradle/profiler/mutations/ApplyChangeToAndroidManifestFileMutator.java
@@ -9,10 +9,17 @@ public class ApplyChangeToAndroidManifestFileMutator extends AbstractFileChangeM
 
     @Override
     protected void applyChangeTo(StringBuilder text) {
-        int insertPos = text.lastIndexOf("</manifest>");
-        if (insertPos < 0) {
-            throw new IllegalArgumentException("Cannot parse android manifest file " + sourceFile + " to apply changes");
+        int insertPos = 0;
+        insertPos = text.lastIndexOf("</manifest>");
+        if (insertPos > 0) {
+            text.insert(insertPos, "<!-- " + getUniqueText() + " --><permission android:name=\"com.acme.SOME_PERMISSION\"/>");
+            return;
         }
-        text.insert(insertPos, "<!-- " + getUniqueText() + " --><permission android:name=\"com.acme.SOME_PERMISSION\"/>");
+        insertPos = text.lastIndexOf("/>");
+        if (insertPos > 0) {
+            text.replace(insertPos, insertPos + 2, " xmlns:android=\"http://schemas.android.com/apk/res/android\"><!-- " + getUniqueText() + " --><permission android:name=\"com.acme.SOME_PERMISSION\"/></manifest>");
+            return;
+        }
+        throw new IllegalArgumentException("Cannot parse android manifest file " + sourceFile + " to apply changes");
     }
 }
